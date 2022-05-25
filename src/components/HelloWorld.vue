69711c71c7af412a70746ce1f461886df0c0c3f2
Jussi Latvaniemi <jussi.latvaniemi@gmail.com>
2022-05-25 14:42:06 +0300

    WIP: playarea and tiles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,51 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  </div>
+  <div class="playarea" :style="cssProps">
+    <Tile v-for="item in items" :key="item.id" :msg="item.id"/>
   </div>
 </template>
 
 <script>
+import Tile from './Tile.vue';
+
 export default {
   name: 'HelloWorld',
+
   props: {
-    msg: String
+    msg: String,
+    numberOfTiles: Number
+  },
+
+  components: {
+    Tile
+  },
+
+  data() {
+    return {
+      items: (() => {
+        const result = [];
+        let counter = 0;
+
+        for (let i = 0; i < this.numberOfTiles; i++) {
+          for (let j = 0; j < this.numberOfTiles; j++) {
+            counter++;
+            result.push({ id: counter });
+          }
+        }
+
+        return result;
+      })()
+    }
+  },
+
+  computed: {
+    cssProps() {
+      return {
+        '--size': (this.numberOfTiles * 2) + "em",
+      }
+    }
   }
 }
 </script>
@@ -54,5 +65,14 @@ li {
 }
 a {
   color: #42b983;
+}
+.playarea {
+  background-color: #eee;
+  height: var(--size);
+  width: var(--size);
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-content: flex-start;
 }
 </style>
---
+++ src/components/Tile.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tile">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'TileComponent',
+  props: {
+    msg: Number
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.tile {
+  height: 2em;
+  width: 2em;
+  background-color: hotpink;
+  flex-shrink: 0;
+}
+</style>
